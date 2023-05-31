7fe753e20aa503397933cda6791de085565778c7
Matheus Pereira <matheusgp.mto@outlook.com>
2023-05-30 23:18:36 -0300

    Criando o esqueleto da API para cadastrar informações de usuário no banco de dados

--- src/app/servidor/API/routes.py
+++ src/app/servidor/API/routes.py
@@ -1,6 +1,12 @@
+from flask import request
 from API import app
 
 
 @app.route("/", methods=["GET", "POST"])
 def api():
     return {"nome": "nome"}
+
+
+@app.route("/cadastrar", methods=["GET", "POST"])
+def cadastrar():
+    return request.data
